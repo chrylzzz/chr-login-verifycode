90cb72ccc6600915d5873a8cf92fd264aa5e285a
chryl <cyl5yy412@gmail.com>
2020-09-09 21:20:39 +0800

    验证码

---
+++ src/main/java/com/chryl/smartadmin/VolidateController.java
@@ -0,0 +1,52 @@
+package com.chryl.smartadmin;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Created by Chr.yl on 2020/9/9.
+ *
+ * @author Chr.yl
+ */
+@Slf4j
+@RestController
+@RequestMapping("smart")
+public class VolidateController {
+    @Autowired
+    private DefaultKaptcha defaultKaptcha;
+
+    @GetMapping("/admin")
+    public void show() {
+
+        String kaptchaText = defaultKaptcha.createText();
+
+        String base64Code = "";
+
+        BufferedImage image = defaultKaptcha.createImage(kaptchaText);
+        ByteArrayOutputStream outputStream = null;
+        try {
+            outputStream = new ByteArrayOutputStream();
+            ImageIO.write(image, "jpg", outputStream);
+            base64Code = Base64.encodeBase64String(outputStream.toByteArray());
+        } catch (Exception e) {
+            log.error("verificationCode exception .{}", e);
+        } finally {
+            if (outputStream != null) {
+                try {
+                    outputStream.close();
+                } catch (Exception e) {
+                    log.error("verificationCode outputStream close exception .{}", e);
+                }
+            }
+        }
+    }
+}
